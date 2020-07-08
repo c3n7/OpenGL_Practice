40afdb87b859277ea0074b99aa5c49ee184852d5
Timothy Karani <kartimothy@gmail.com>
2020-07-08 19:10:53 +0300

    Did the many cubes example

--- source/1.Getting_Started/6.Coordinate_Systems/3.Many_Cubes.cpp
+++ source/1.Getting_Started/6.Coordinate_Systems/3.Many_Cubes.cpp
@@ -77,6 +77,19 @@ float vertices[] = {
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
+
+glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(2.0f, 5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f, 3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f, 2.0f, -2.5f),
+    glm::vec3( 1.5f, 0.2f, -1.5f),
+    glm::vec3(-1.3f, 1.0f, -1.5f)
+};
 // clang-format on
 
 int main() {
@@ -209,12 +222,8 @@ int main() {
 
         ourShader.use();
 
-        glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-        model = glm::rotate(model,
-                (float)glfwGetTime() * glm::radians(50.0f),
-                glm::vec3(0.5f, 1.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f),
                 (float)screenWidth / (float)screenHeight,
@@ -227,7 +236,6 @@ int main() {
                 glGetUniformLocation(ourShader.programID, "view");
         unsigned int projectionLoc =
                 glGetUniformLocation(ourShader.programID, "projection");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(
                 projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
@@ -238,7 +246,18 @@ int main() {
         glBindTexture(GL_TEXTURE_2D, textures[1]);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * (i == 0 ? 3 : i);
+
+            model = glm::rotate(model,
+                    (float)glfwGetTime() * glm::radians(angle),
+                    glm::vec3(1.0f, 0.3f, 0.5f));
+
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         glBindVertexArray(0);
 
         // Check and call events and swap the buffer
